35dd46b2a49ce2c0452d3c68460fd26af95b449d
mordhau5 <carl.campbell@placewise.com>
2014-04-03 15:19:40 -0600

    Preliminary randomly generated sequences implemented

--- Bombomb.ino
+++ Bombomb.ino
@@ -8,6 +8,44 @@ int btnPin = 7;
 const int nLeds = 6;
 int ledPin[nLeds] = {11,10,9,6,5,3};
 boolean lastBtn;
+const float fpms = 0.020;
+const int rows = 30;
+const int cols = 8;
+const int finalEvt = rows-1;
+float highVal = 255;
+float lowVal = 0;
+float sequence[rows][cols]; //= {
+	/*Event: 
+	initial value, final value, event runtime(ms)(delta), event begin(ms)(timeline),	<--Given Vals
+	step value, time between ops(ms)(delta), last op time(ms)(timeline), 				<--Calced Vals (below)
+	ledPin#
+	*/
+	/*
+	{0, 255, 1500, 0, 0, 0, 0, -1}, 
+	{0, 50, 1000, 500, 0, 0, 0, -1}, 
+	{40, 167, 3050, 1250, 0, 0, 0, -1},
+	{167, 0, 3050, 3250, 0, 0, 0, -1},
+	{50, 167, 1500, 3950, 0, 0, 0, -1},
+	{255, 20, 5050, 4000, 0, 0, 0, -1},
+	{255, 0, 2000, 4250, 0, 0, 0, -1},
+	{0, 200, 2400, 5250, 0, 0, 0, -1},
+	{0, 200, 2400, 5250, 0, 0, 0, -1},
+	{0, 200, 2400, 5250, 0, 0, 0, -1},
+	{0, 200, 2400, 5250, 0, 0, 0, -1},
+	{0, 200, 2400, 5250, 0, 0, 0, -1}*/
+	/*
+	{0, highVal, 1500, 0, 0, 0, 0, 0}, 
+	{0, highVal, 1500, 0, 0, 0, 0, 2}, 
+	{0, highVal, 1500, 0, 0, 0, 0, 4},
+	{highVal, 0, 1950, 2000, 0, 0, 0, 0},
+	{highVal, 0, 1950, 2000, 0, 0, 0, 2},
+	{highVal, 0, 1950, 2000, 0, 0, 0, 4},
+	{0, highVal, 1950, 3000, 0, 0, 0, 1},
+	{0, highVal, 1950, 3000, 0, 0, 0, 3},
+	{0, highVal, 1950, 3000, 0, 0, 0, 5},
+	{highVal, 0, 500, 5200, 0, 0, 0, 1},
+	{highVal, 0, 500, 5200, 0, 0, 0, 3},
+	{highVal, 0, 500, 5200, 0, 0, 0, 5}*/
 
 void setup(){
 	//give the body declared hardware
@@ -15,7 +53,7 @@ void setup(){
 	for(int i=0;i<nLeds;i++){
 		pinMode(ledPin[i], OUTPUT);
 	}
-	
+	Serial.begin(9600);
 	lastBtn = LOW;
 }
 
@@ -24,6 +62,7 @@ void loop(){
 	if(btnPushed && !lastBtn){
 		delay(5);
 		lastBtn = HIGH;
+		createRandomSequence();
 		doOps();
 	}
 	else{
@@ -39,44 +78,25 @@ perform operations on.  It will need to be able to switch between Events quickly
 process them quick enough for real time.
 *****/
 void doOps(){
-	Serial.begin(9600);
-	const float fpms = 0.020;
-	const int rows = 12;
-	const int cols = 8;
-	const int finalEvt = rows-1;
-	float highVal = 50;
-	float lowVal = 0;
-	float sequence[rows][cols] = {
-		/*Event: 
-		initial value, final value, event runtime(ms)(delta), event begin(ms)(timeline),	<--Given Vals
-		step value, time between ops(ms)(delta), last op time(ms)(timeline), 				<--Calced Vals (below)
-		ledPin#
-		*/
-		/*{0, 255, 1500, 0, 0, 0, 0, 0}, 
-		{0, 50, 1000, 500, 0, 0, 0, 2}, 
-		{40, 167, 3050, 1250, 0, 0, 0, 1},
-		{167, 0, 3050, 3250, 0, 0, 0, 1},
-		{50, 167, 1500, 3950, 0, 0, 0, 2},
-		{255, 20, 5050, 4000, 0, 0, 0, 3},
-		{255, 0, 2000, 4250, 0, 0, 0, 0},
-		{0, 200, 2400, 5250, 0, 0, 0, 4}*/
-		{0, highVal, 1500, 0, 0, 0, 0, 0}, 
-		{0, highVal, 1500, 0, 0, 0, 0, 2}, 
-		{0, highVal, 1500, 0, 0, 0, 0, 4},
-		{highVal, 0, 2000, 2000, 0, 0, 0, 0},
-		{highVal, 0, 2000, 2000, 0, 0, 0, 2},
-		{highVal, 0, 2000, 2000, 0, 0, 0, 4},
-		{0, highVal, 2000, 3000, 0, 0, 0, 1},
-		{0, highVal, 2000, 3000, 0, 0, 0, 3},
-		{0, highVal, 2000, 3000, 0, 0, 0, 5},
-		{highVal, 0, 500, 5000, 0, 0, 0, 1},
-		{highVal, 0, 500, 5000, 0, 0, 0, 3},
-		{highVal, 0, 500, 5000, 0, 0, 0, 5}
-	};
+	
 	for(int i=0;i<rows;i++){//determine step values and time between ops (so I don't need to for every new test case)
 		sequence[i][4] = (sequence[i][1] - sequence[i][0]) * fpms;
 		sequence[i][5] = sequence[i][2] * fpms;
 	}
+
+	//Show sequence here
+	Serial.println("Executing sequence {");
+	for(int i=0;i<rows;i++){
+		Serial.print("[");
+		Serial.print((int)sequence[i][0]);
+		for(int j=1;j<cols;j++){
+			Serial.print(", ");
+			Serial.print((int)sequence[i][j]);
+		}
+		Serial.println("],");
+	}
+	Serial.println("}");
+	
 	
 	long timeCurr, timeStart = millis();
 	Serial.print(">Begin @ ");
@@ -91,39 +111,26 @@ void doOps(){
 	** 		 ^-> Plan test cases accordingly
 	*/
 		timeCurr = millis() - timeStart;
-		if((float)timeCurr >= sequence[i][3]){ //is it time to start this event?
-			
-			// Serial.print((long)sequence[i][5]);
-			// Serial.print(" ");
-			// Serial.println(timeCurr);
-			/*is this event still running && has the given amount of time passed since this event last had an op ?*/
+		if((float)timeCurr >= sequence[i][3]){
 			timeCurr = millis() - timeStart;
 			if( (sequence[i][0] != sequence[i][1]) && ( (float)timeCurr - sequence[i][6] >= sequence[i][5] ) ) { 
-				// Serial.print(i);
-				/*this would need to change for other types of operations*/
+				/*Serial.print(i);
+				Serial.print(" @ ");
+				Serial.println(timeCurr);*/
 				float rawVal = sequence[i][0] + sequence[i][4];
 				if(sequence[i][4] < 0)
-					sequence[i][0] = constrain(floor(rawVal),sequence[i][1],sequence[i][0]); //do negative op
+					sequence[i][0] = constrain(rawVal,sequence[i][1],sequence[i][0]);
 				else
-					sequence[i][0] = constrain(floor(rawVal),sequence[i][0],sequence[i][1]); //do positive op
-				analogWrite(ledPin[(int)sequence[i][7]], sequence[i][0]);
+					sequence[i][0] = constrain(rawVal,sequence[i][0],sequence[i][1]);
+				analogWrite(ledPin[(int)sequence[i][7]], (int)sequence[i][0]);
 				timeCurr = millis() - timeStart;
-				sequence[i][6] = (float)timeCurr; //store op time
-				// Serial.println(rawVal);
-				// Serial.println(timeCurr/1000.0); //Can't concatenate with FL/DBL by default :(
-				//Serial.println(sequence[i][3]);
-				 // Serial.print(constrain(sequence[i][0]+sequence[i][3],sequence[i][0],sequence[i][1]));
-				// Serial.println(sequence[i][5]);
-				  // Serial.println(sequence[i][6]);
-				/*Serial commands slow things down immensely!*/
+				sequence[i][6] = (float)timeCurr;
 			}
 			/*maybe remove event if it's completed? Hopefully in finalized DS*/
 		}
-		else {
-			// Serial.println("so this happened");
-			i = -1; //can't start this event in the *ordered* sequence, no sense in going forward.  Optimization measure.
-		}
-		if(i == finalEvt) //We're at the last event, start back at first one. OH WAIT THIS IS NOT TRUE WHEN EARLIER EVENTS HAVE COMPLETED <-- but this check also might not be helpful
+		else
+			i = -1; //Optimization measure
+		if(i == finalEvt) //THIS IS NOT TRUE WHEN EARLIER EVENTS HAVE COMPLETED <-- but this check also might not be helpful
 			i=0;
 		else
 			i++;
@@ -131,4 +138,59 @@ void doOps(){
 
 	Serial.print("Finished in ");
 	Serial.println(millis() - timeStart);
+
+	//zero out sequence and peripherals
+	for(int i=0;i<rows;i++){
+		int pinNum = sequence[i][7];
+		analogWrite(ledPin[pinNum], 0);
+		sequence[i][7]=-1;
+	}
+}
+
+void createRandomSequence(){
+	long seed = analogRead(0) * millis(); //Read input on unconnected pin and time
+	randomSeed(seed);
+	Serial.print("Seed: ");
+	Serial.println(seed);
+	long timeStart = millis();
+	int peripheralID, lastEventOnPID;
+	int initValue;
+	for(int i=0;i<rows;i++){
+		peripheralID = random(nLeds);
+		lastEventOnPID = lastEventOnPeripheral(peripheralID, i);
+		//Event Initial Value
+		if(lastEventOnPID < 0)//No Event found for this LED
+			sequence[i][0] = random(highVal);
+		else//set it's inital value to the final value of the last event
+			sequence[i][0] = sequence[lastEventOnPID][1];
+		//Event Final Value
+		sequence[i][1] = random(highVal);
+		//Event Runtime
+		sequence[i][2] = random(500,5000);
+		//Event Start Time
+		if(lastEventOnPID >= 0)
+			sequence[i][3] = max(sequence[lastEventOnPID][2] + sequence[lastEventOnPID][3] + 250,sequence[i-1][3]);
+		else if(i > 0)
+			sequence[i][3] = sequence[i-1][3];
+		else
+			sequence[i][3] = 0;
+		//Event Peripheral ID
+		sequence[i][4] = 0;
+		sequence[i][5] = 0;
+		sequence[i][6] = 0;
+		sequence[i][7] = peripheralID;
+	}
+	Serial.print("Sequence generated in: ");
+	Serial.println(millis() - timeStart);
+}
+
+int lastEventOnPeripheral(int pid, int startIndex){
+	int pos = -1;
+	for(int i=startIndex-1;i>=0;i--){
+		if(sequence[i][7] == pid){
+			pos = i;
+			i = 0;
+		}
+	}
+	return pos;
 }
